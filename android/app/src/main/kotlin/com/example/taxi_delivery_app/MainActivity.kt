7d6d4ebdef0fe004484ef6bd9c6500055780fafb
Zero_gi <rob.zgonnikov@gmail.com>
2021-05-01 17:39:39 +0300

    add map

--- android/app/src/main/kotlin/com/example/taxi_delivery_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/taxi_delivery_app/MainActivity.kt
@@ -1,6 +1,15 @@
 package com.example.taxi_delivery_app
 
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+import com.yandex.mapkit.MapKitFactory
+
 
 class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        MapKitFactory.setApiKey("c184abe1-6d6f-4c22-a2a5-9e778f65d9a8")
+        super.configureFlutterEngine(flutterEngine)
+    }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:taxi_delivery_app/screens/LoginScreen.dart';
+import 'package:yandex_mapkit/yandex_mapkit.dart';
 
 
+/*
 void main(){
   runApp(
       MaterialApp(
         home:LogIn()));
+}
+*/
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MaterialApp(
+      title: "Map Demo",
+      theme: ThemeData(
+          primarySwatch: Colors.blue
+      ),
+      home: HomePage()));
+}
+
+class HomePage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Expanded(
+              child: YandexMap()
+          )
+        ],
+      ),
+    );
+  }
 }
