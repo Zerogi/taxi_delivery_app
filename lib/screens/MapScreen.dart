7eca778a67e990fe0803091890553e19fca73cbd
Zero_gi <rob.zgonnikov@gmail.com>
2021-05-01 23:48:32 +0300

    main menu added

---
+++ lib/screens/MapScreen.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:taxi_delivery_app/screens/LoginScreen.dart';
+import 'package:yandex_mapkit/yandex_mapkit.dart';
+import 'package:taxi_delivery_app/screens/LoginScreen.dart';
+
+
+class HomePage extends StatelessWidget{
+  YandexMapController controller;
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+
+        children: <Widget>[
+          Expanded(
+            flex: 10,
+              child: YandexMap(
+                onMapCreated: (YandexMapController yandexMapController) async{
+                  controller = yandexMapController;
+                  final Point currentTarget = await controller.enableCameraTracking(
+                      const PlacemarkStyle(iconName: 'lib/assets/place.png', opacity: 0.8),
+                      cameraPositionChanged);
+                  await addUserPlacemark(currentTarget);
+                },
+              )
+          ),
+
+          Expanded(
+              flex: 1,
+              //fit:FlexFit.tight,
+              child: FractionallySizedBox(widthFactor: 1, heightFactor: 1,
+                  child:MaterialButton(
+                      disabledColor: Colors.grey,
+                      disabledTextColor: Colors.black87,
+                      color: Colors.redAccent,
+                      textColor: Colors.white,
+                      minWidth: double.infinity,
+                      child: FittedBox(fit:BoxFit.contain,
+                          child:Text("Заказать",
+                              style: TextStyle(color: Colors.white))),
+                      onPressed: (){
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context)=>LogIn()));
+                      }
+                  ))),
+        ],
+      ),
+    );
+  }
+
+  Future<void> cameraPositionChanged(dynamic arguments) async {
+    final bool bFinal = arguments['final'];
+    if (bFinal) {
+      await addUserPlacemark(Point(
+          latitude: arguments['latitude'],
+          longitude: arguments['longitude']
+      ));
+    }
+  }
+
+  Future<void> addUserPlacemark(Point point) async {
+    await controller.addPlacemark(Placemark(
+      point: point,
+      style: const PlacemarkStyle(
+        opacity: 0.1, // 0.9
+      ),
+    ));
+  }
+}
---
+++ lib/target.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:yandex_mapkit/yandex_mapkit.dart';
+
+class _TargetExample extends StatefulWidget {
+  @override
+  _TargetExampleState createState() => _TargetExampleState();
+}
+
+class _TargetExampleState extends State<_TargetExample> {
+  YandexMapController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(
+              child: YandexMap(
+                onMapCreated: (YandexMapController yandexMapController) async {
+                  controller = yandexMapController;
+                },
+              )
+          ),
+          const SizedBox(height: 20),
+          Expanded(
+              child: SingleChildScrollView(
+                  child: Column(
+                      children: <Widget>[
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: <Widget>[
+                            ControlButton(
+                                onPressed: () async {
+                                  final Point currentTarget = await controller!.enableCameraTracking(
+                                      const PlacemarkStyle(iconName: 'lib/assets/place.png', opacity: 0.5),
+                                      cameraPositionChanged
+                                  );
+                                  await addUserPlacemark(currentTarget);
+                                },
+                                title: 'Tracking'
+                            ),
+                            ControlButton(
+                                onPressed: () async {
+                                  final Point currentTarget = await controller!.enableCameraTracking(null, cameraPositionChanged);
+                                  await addUserPlacemark(currentTarget);
+                                },
+                                title: 'Tracking (without marker)'
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 10),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: <Widget>[
+                            ControlButton(
+                                onPressed: () async {
+                                  await controller!.disableCameraTracking();
+                                },
+                                title: 'Disable tracking'
+                            ),
+                          ],
+                        ),
+                      ]
+                  )
+              )
+          )
+        ]
+    );
+  }
+
+  Future<void> cameraPositionChanged(dynamic arguments) async {
+    final bool bFinal = arguments['final'];
+    if (bFinal) {
+      await addUserPlacemark(Point(
+          latitude: arguments['latitude'],
+          longitude: arguments['longitude']
+      ));
+    }
+  }
+
+  Future<void> addUserPlacemark(Point point) async {
+    await controller!.addPlacemark(Placemark(
+      point: point,
+      style: const PlacemarkStyle(
+        iconName: 'lib/assets/user.png',
+        opacity: 0.9,
+      ),
+    ));
+  }
+}
